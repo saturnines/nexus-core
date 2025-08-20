30d83fda11bc02635199b04a41a0160c8f41c1b5
Kevin <leungke@oregonstate.edu>
2025-08-20 08:28:27 -0500

    Update builder.go
    added regex support to expand var

--- pkg/transport/rest/builder.go
+++ pkg/transport/rest/builder.go
@@ -1,8 +1,12 @@
+// pkg/transport/rest/builder.go
 package rest
 
 import (
 	"context"
 	"net/http"
+	"os"
+	"regexp"
+	"strings"
 
 	"github.com/saturnines/nexus-core/pkg/auth"
 )
@@ -37,19 +41,24 @@ func NewBuilder(
 
 // Build creates an HTTP request.
 func (b *Builder) Build(ctx context.Context) (*http.Request, error) {
-	req, err := http.NewRequestWithContext(ctx, b.Method, b.URL, nil)
+	// Substitute template variables in the URL
+	url := b.substituteTemplateVariables(b.URL)
+	
+	req, err := http.NewRequestWithContext(ctx, b.Method, url, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	for k, v := range b.Headers {
-		req.Header.Set(k, v)
+		// Also substitute template variables in header values
+		req.Header.Set(k, b.substituteTemplateVariables(v))
 	}
 
 	if len(b.QueryParams) > 0 {
 		q := req.URL.Query()
 		for k, v := range b.QueryParams {
-			q.Set(k, v)
+			// Also substitute template variables in query param values
+			q.Set(k, b.substituteTemplateVariables(v))
 		}
 		req.URL.RawQuery = q.Encode()
 	}
@@ -61,4 +70,21 @@ func (b *Builder) Build(ctx context.Context) (*http.Request, error) {
 	}
 
 	return req, nil
+}
+
+// substituteTemplateVariables replaces {{VAR_NAME}} with environment variable values
+func (b *Builder) substituteTemplateVariables(text string) string {
+	// This regex matches {{VARIABLE_NAME}} patterns
+	templatePattern := regexp.MustCompile(`\{\{([^}]+)\}\}`)
+	
+	return templatePattern.ReplaceAllStringFunc(text, func(match string) string {
+		// Extract variable name (remove {{ and }})
+		varName := strings.TrimSpace(match[2 : len(match)-2])
+		
+		// Get environment variable value
+		if value := os.Getenv(varName); value != "" {
+			return value
+		}
+		return match
+	})
 }
